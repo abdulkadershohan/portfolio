4441b067be3e1113d65e8a1af39b4bb843485261
Abdul Kader <56482597+abdulkadershohan@users.noreply.github.com>
2020-10-19 21:51:15 +0600

    Updates

---
+++ precache-manifest.6335f1727d9d17512affb5426644edf1.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e59ba6febce8f71d2689d367717ad77",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3c790acbe2d9f3c66127",
+    "url": "/portfolio/static/css/main.65f16a7d.chunk.css"
+  },
+  {
+    "revision": "5c98d108b532078e7b28",
+    "url": "/portfolio/static/js/2.773c9172.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.773c9172.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c790acbe2d9f3c66127",
+    "url": "/portfolio/static/js/main.ec9d3805.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "a92954f2d2f5cacd5c17e6e89b647388",
+    "url": "/portfolio/static/media/About.a92954f2.jpg"
+  },
+  {
+    "revision": "87380d309c90a404f19e62ab423246cd",
+    "url": "/portfolio/static/media/Back-end.87380d30.jpg"
+  },
+  {
+    "revision": "f0b26fb11be83b28cd3fd6f8c4c47a28",
+    "url": "/portfolio/static/media/bg2.f0b26fb1.jpg"
+  },
+  {
+    "revision": "3910d5e7dbf3f6bb02298a085054d1da",
+    "url": "/portfolio/static/media/font-end.3910d5e7.png"
+  },
+  {
+    "revision": "94e5eb46806bcd9c85e797db3e72322e",
+    "url": "/portfolio/static/media/mobile.94e5eb46.jpg"
+  },
+  {
+    "revision": "4de529b7346499b6d948e35cfdb08145",
+    "url": "/portfolio/static/media/project-1.4de529b7.jpg"
+  },
+  {
+    "revision": "a0d80280354b30bbc3614e8c78323255",
+    "url": "/portfolio/static/media/project-2.a0d80280.jpg"
+  }
+]);
